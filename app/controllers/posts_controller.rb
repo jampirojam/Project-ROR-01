b4e296156fd7da2aba2aefd43c0ff37684213d65
M. Machrush Aliy Sirojjam Mushlich <jampirojam@gmail.com>
2022-03-21 22:02:40 +0700

    init

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,11 @@
 class PostsController < ApplicationController
   def index
     # Paste the posts array defined in the view, then rename it to @posts
-      @posts = [
-        "Learning Rails with Progate!",
-        "Trying to display the posts!"
-      ]
+    #  @posts = [
+    #    "Learning Rails with Progate!",
+    #    "Trying to display the posts!",
+    #  ]
+    
+    @posts = Post.all
   end
 end
