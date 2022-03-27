125f2167cfe767a0c8a2f4a2844de87eb2d01ba3
M. Machrush Aliy Sirojjam Mushlich <jampirojam@gmail.com>
2022-03-28 03:50:15 +0700

    init

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,18 +8,33 @@ class UsersController < ApplicationController
   end
   
   def new
-    # Declare the @user variable and assign it the a new instance of User
     @user = User.new
   end
   
   def create
     @user = User.new(name: params[:name], email: params[:email])
-    # Redirect to the Users page if the @user is valid, and render the New user page if it isn't
     if @user.save
       flash[:notice] = "You have signed up successfully"
       redirect_to("/users/#{@user.id}")
     else
       render("users/new")
+    end
+  end
+  
+  def edit
+    @user = User.find_by(id: params[:id])
+  end
+  
+  # Add a new action named update
+  def update
+    @user = User.find_by(id: params[:id])
+    @user.name = params[:name]
+    @user.email = params[:email]
+    if @user.save
+      flash[:notice] = "Your account has been updated successfully"
+      redirect_to("/users/#{@user.id}")
+    else
+      render("users/edit")
     end
   end
   
