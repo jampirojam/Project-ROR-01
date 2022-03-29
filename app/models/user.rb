02a5670e2e2979f6183d80a660e9cc8b28aff850
M. Machrush Aliy Sirojjam Mushlich <jampirojam@gmail.com>
2022-03-30 05:39:35 +0700

    init

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,12 @@
 class User < ApplicationRecord
+  # Add the has_secure_password method
+  has_secure_password
+  
   validates :name, {presence: true}
   validates :email, {presence: true, uniqueness: true}
-  validates :password, {presence: true}
+  # Remove the following validation
   
-  # Define the "posts" instance method
+  
   def posts
     return Post.where(user_id: self.id)
   end
